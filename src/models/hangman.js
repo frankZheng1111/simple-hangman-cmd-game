585b82133fcfe8f609f75f11354cb850d31a8d27
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-03-31 15:05:32 +0800

    Add Hangman class

--- src/models/hangman.js
+++ src/models/hangman.js
@@ -3,14 +3,39 @@ import { HangManError } from '../errors';
 
 class HangMan {
   constructor() {
-    this.word = "laevatein";
-    this.wordLetters = this.word.split("");
+    this.protoWord = "laevatein".toLowerCase();
+    this.word = this.protoWord.replace(/[a-z]/g, '*');
+    this.wordLetters = this.protoWord.split("");
     this.guessedLetters = [];
     this.hp = 10;
   }
 
+  get protoWord() {
+    return protoWord;
+  }
+
   guess(letter) {
-    throw new HangManError("input.guessed.letter");
+    if (letter.length > 1) { throw new HangManError("input.multi.letters"); }
+    if (letter.length < 1) { throw new HangManError("input.nothing"); }
+    if (this.guessedLetters.includes(letter)) { throw new HangManError("input.guessed.letter"); }
+    this.guessedLetters.push(letter);
+    if (!this.wordLetters.includes(letter)) { this.hp--; }
+    this.word = wordLetters.map((letter) {
+      if (this.guessedLetters.includes(letter)) {
+        return letter;
+      } else {
+        return "*";
+      }
+    }).join('');
+    return this.word;
+  }
+
+  isAlive() {
+    return this.hp > 0;
+  }
+
+  isWin() {
+    return !this.word.split('').includes('*');
   }
 }
 
