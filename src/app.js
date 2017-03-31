2e16ea7ae9bfd495e84bfd222dec2d60daee5b74
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-03-31 10:53:16 +0800

    Add logger and readlinesync

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
 "use strict"
-console.log('hangman')
+import readlineSync from "readline-sync";
+
+import logger from "./lib/logger"
+
+let userName = readlineSync.question('May I have your name? ');
+
+console.log('Game start ~');
+console.log(`Hi ${userName}~`);
---
+++ src/lib/logger.js
@@ -0,0 +1,29 @@
+"use strict"
+import log4js from "log4js";
+
+log4js.configure({
+  appenders: [
+    {
+      type: "logLevelFilter",
+      level: "INFO",
+      appender: {
+        type: "console",
+      }
+    }, //控制台输出
+    {
+      type: "logLevelFilter",
+      level: "DEBUG",
+      appender: {
+        type: "dateFile", //文件输出
+        filename: "log/hangman",
+        alwaysIncludePattern: true,
+        pattern: "-yyyy-MM-dd.log",
+        category: "normal"
+      }
+    }
+  ],
+  replaceConsole: true
+});
+
+let logger = log4js.getLogger("normal");
+export default logger;
