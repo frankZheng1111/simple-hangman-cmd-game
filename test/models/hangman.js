2d91924dee4f6eea07194cb5303d4f5e239773b2
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-04-01 15:07:09 +0800

    Add more test

--- test/models/hangman.js
+++ test/models/hangman.js
@@ -5,24 +5,46 @@ let should = _should();
 
 describe('Test HangMan Class', () => {
   let hangman = {}
-  before(() => {
+  beforeEach(() => {
     hangman = new HangMan();
   });
 
-  it('should return hp', () => {
-    hangman.hp.should.equal(10);
+  describe('test constructor', () => {
+    it('should return hp', () => {
+      hangman.hp.should.equal(10);
+    });
+
+    it('word length should equal protoWord length', () => {
+      hangman.protoWord.length.should.equal(hangman.word.length);
+    });
+
+    it('guessWords should be empty', () => {
+      hangman.guessedLetters.length.should.equal(0);
+    });
   });
 
-  it('word length should equal protoWord length', () => {
-    hangman.protoWord.length.should.equal(hangman.word.length);
+  describe('judge whether win', () => {
+    it('should return true', () => {
+      hangman.word = "aaa";
+      hangman.isWin().should.equal(true);
+    });
+
+    it('should return false', () => {
+      hangman.word = "***";
+      hangman.isWin().should.equal(false);
+    });
   });
 
-  it('guessWords should be empty', () => {
-    hangman.guessedLetters.length.should.equal(0);
+  describe('judge whether Alive', () => {
+    it('should return true', () => {
+      hangman.isAlive().should.equal(true);
+    });
+
+    it('should return false', () => {
+      hangman.hp = 0;
+      hangman.isAlive().should.equal(false);
+    });
   });
 
 });
 
-exports.covered = {
-    'is bar': () => assert.equal(covered, 'bar'),
-}
